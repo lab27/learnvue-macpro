b45b6aa5128f7f602f4ca2df124215763e89eeeb
Nick Niles <nickyniles@gmail.com>
2017-04-12 22:55:44 +0200

    begin the begin

---
+++ main.js
@@ -0,0 +1,64 @@
+Vue.component('message', {
+
+    props: ['title', 'body', 'extraclass'],
+    data() {
+        return {
+            isVisible: true
+        };
+    },
+    template: `
+    <article class="message" :class="extraclass" v-show="isVisible">
+  <div class="message-header">
+    <p>{{title}}</p>
+    <button class="delete" @click="isVisible = false"></button>
+  </div>
+  <div class="message-body">
+  {{body}}
+  </div>
+</article>
+    `,
+   
+    methods: {
+        hideModal() {
+            this.isVisible = false;
+        }
+    }
+
+});
+
+
+Vue.component('modal', {
+
+    template: `
+    
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+            
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Aliquam, enim quaerat inventore a quasi? Quia dolor facere numquam ab totam?
+            </div>
+        </div>
+        <button class="modal-close" @click="$emit('close')"></button>
+    </div>
+
+
+    `,
+    methods: {
+        showModal() {
+            this.isVisible = false;
+        }
+    }
+
+
+});
+
+
+
+new Vue({
+el: '#root',
+data: {
+    showModal: false
+}
+});
